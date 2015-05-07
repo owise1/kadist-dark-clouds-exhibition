643550e16239378e0134c853c1a2e563b3bdff1b
Oliver Wise <owise1@gmail.com>
2015-05-07 13:24:19 -0700

    add master

--- update-exhibition.sh
+++ update-exhibition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/pi/kadist-dark-clouds-exhibition
-git pull origin
+git pull origin master
 couchapp push app/ http://localhost:5984/liblib
 echo "^if everything is ok you should see a strange URL above ^"
